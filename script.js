92829beafa7d1fec8e316e4d7e1ba62e88dc59cf
CarolinaValcarce <carolina.valarce@hotmail.com>
2021-09-05 20:32:09 +0200

    Second upload

---
+++ script.js
@@ -0,0 +1,293 @@
+const quizData=[{
+	question:"Cuál es el lenguaje de programación más usado en el 2020?",
+	answer_a: "Python",
+	answer_b: "Java",
+	answer_c: "Javascript",
+	answer_d: "C#",
+	correct_answer: "c",
+},{
+	question:"Cúal de estos animales no vive en África?",
+	answer_a: "Leopardo",
+	answer_b: "Jirafa",
+	answer_c: "Guepardo",
+	answer_d: "Tigre",
+	correct_answer: "d",
+},{
+	question:"De qué está hecho el vinagre balsámico tradicional?",
+	answer_a: "Jarabe de maíz",
+	answer_b: "Uvas",
+	answer_c: "Manzanas",
+	answer_d: "Manteca de cerdo",
+	correct_answer: "b",
+},{
+	question:"Cuál de estos factores contribuyen a la formación de sedimentos eólicos?",
+	answer_a: "Vientos",
+	answer_b: "Glaciares",
+	answer_c: "Mareas",
+	answer_d: "Ríos",
+	correct_answer: "a",
+},{
+	question:"Qué cosa se almacena en la joroba de un camello?",
+	answer_a: "Grasa",
+	answer_b: "Fibra",
+	answer_c: "Agua",
+	answer_d: "Azúcar",
+	correct_answer: "a",
+},{
+	question:"Cuál de estos es un pueblo Turquico?",
+	answer_a: "Anuto",
+	answer_b: "Moken",
+	answer_c: "Tártaro",
+	answer_d: "Pama-nyunga",
+	correct_answer: "c",
+},{
+	question:"Qué es un esturión kaluga?",
+	answer_a: "Mamífero",
+	answer_b: "Pez",
+	answer_c: "Ave",
+	answer_d: "Insecto",
+	correct_answer: "b",
+},{
+	question:"Cuál de estos es un receptáculo para recoger y guardar el agua?",
+	answer_a: "Cisterna",
+	answer_b: "Molino",
+	answer_c: "Caserna",
+	answer_d: "Atril",
+	correct_answer: "a",
+},{
+	question:"Qué tipo de animal es un puercospín?",
+	answer_a: "Marsupial",
+	answer_b: "Roedor",
+	answer_c: "Musaraña",
+	answer_d: "Rumiante",
+	correct_answer: "b",
+},{
+	question:"Cuál de estas plantas comen los koalas?",
+	answer_a: "Manzana",
+	answer_b: "Arroz",
+	answer_c: "Eucalipto",
+	answer_d: "Bambú",
+	correct_answer: "c",
+},{
+	question:"Cuál es la capital de Venezuela?",
+	answer_a: "Lima",
+	answer_b: "Rio de Janeiro",
+	answer_c: "Buenos Aires",
+	answer_d: "Caracas",
+	correct_answer: "d",
+},{
+	question:"Quién es el autor de el Don Quijote de la Mancha?",
+	answer_a: "Machado",
+	answer_b: "Cervantes",
+	answer_c: "Becquer",
+	answer_d: "Zola",
+	correct_answer: "b",
+},{
+	question:"Quién fue el primer hombre en pisar la luna?",
+	answer_a: "Armstrong",
+	answer_b: "Conrad",
+	answer_c: "Young",
+	answer_d: "Shepard",
+	correct_answer: "a",
+},{
+	question:"Cuándo acabó la Segunda Guerra Mundial?",
+	answer_a: "1955",
+	answer_b: "1945",
+	answer_c: "1946",
+	answer_d: "1944",
+	correct_answer: "b",
+},{
+	question:"Quién fue la primera mujer en ir al espacio?",
+	answer_a: "Tereshkova",
+	answer_b: "Smokva",
+	answer_c: "Smirnof",
+	answer_d: "Petrov",
+	correct_answer: "a",
+},{
+	question:"Qué rama de la Biología se encarga del estudio de los animales?",
+	answer_a: "Zoología",
+	answer_b: "Botánica",
+	answer_c: "Ecología",
+	answer_d: "Etnología",
+	correct_answer: "a",
+},{
+	question:"A qué personas se las conoce como cariocas?",
+	answer_a: "A los originarios de Río de Janeiro ",
+	answer_b: "A los originarios de Río de México",
+	answer_c: "A los originarios de Río de Lima",
+	answer_d: "A los originarios de Santiago de Chile",
+	correct_answer: "a",
+},{
+	question:"En qué país se encuentra el Wembley Arena?",
+	answer_a: "Bélgica concretamente Bruselas ",
+	answer_b: "Inglaterra concretamente Londres",
+	answer_c: "España concretamente Barcelona",
+	answer_d: "Francia concretamente Paris",
+	correct_answer: "b",
+},{
+	question:"Cuáles son los cinco tipos de sabores primarios?",
+	answer_a: "Los sabores primarios son dulce, amargo, ácido, salado y seco.",
+	answer_b: "Los sabores primarios son dulce, amargo, ácido, salado y umami.",
+	answer_c: "Los sabores primarios son dulce, amargo, ácido, salado y reacio.",
+	answer_d: "Los sabores primarios son dulce, amargo, ácido, salado y utami.",
+	correct_answer: "b",
+},{
+	question:"Cómo se llama la capital de Mongolia?",
+	answer_a: "Abiyán",
+	answer_b: "Ulam Bator",
+	answer_c: "Luanda",
+	answer_d: "Nairobi",
+	correct_answer: "b",
+},{
+	question:"Cuál es el río más largo del mundo?",
+	answer_a: "Mississipi",
+	answer_b: "Danubio",
+	answer_c: "Amazonas",
+	answer_d: "Nilo",
+	correct_answer: "d",
+},{
+	question:"Qué tipo de animal es la ballena?",
+	answer_a: "Pez",
+	answer_b: "Crustáceo",
+	answer_c: "Mamífero",
+	answer_d: "Molusco",
+	correct_answer: "c",
+},{
+	question:"De qué colores es la bandera de México?",
+	answer_a: "Verde, roja y blanca.",
+	answer_b: "Verde, blanca y naranja.",
+	answer_c: "Verde y blanca.",
+	answer_d: "Verde y roja",
+	correct_answer: "a",
+},{
+	question:"Qué país no tiene en su bandera los  colores verde, blanco y rojo?",
+	answer_a: "Italia",
+	answer_b: "Hungría",
+	answer_c: "México",
+	answer_d: "India",
+	correct_answer: "d",
+},{
+	question:"Quién pintó “la última cena?",
+	answer_a: "Velázquez",
+	answer_b: "Da Vinci",
+	answer_c: "Goya",
+	answer_d: "Van Gogh",
+	correct_answer: "b",
+},{
+	question:"Cuál es el océano más grande?",
+	answer_a: "Ártico",
+	answer_b: "Índico",
+	answer_c: "Pacífico",
+	answer_d: "Atlántico",
+	correct_answer: "b",
+},{
+	question:"Quién es el padre del psicoanálisis?",
+	answer_a: "Freud",
+	answer_b: "Kant",
+	answer_c: "Hegel",
+	answer_d: "Pavlov",
+	correct_answer: "a",
+},{
+	question:"Cuál es el disco más vendido de la historia?",
+	answer_a: "Imagine de Los Beathes",
+	answer_b: "Thriller de M Jackson",
+	answer_c: "La Isla bonita de Madonna",
+	answer_d: "El guardaspaldas de W Houston",
+	correct_answer: "a",
+},{
+	question:"Qué producto cultiva más Guatemala?",
+	answer_a: "Marihuana",
+	answer_b: "Caña",
+	answer_c: "Maíz",
+	answer_d: "Café",
+	correct_answer: "d",
+},{
+	question:"Qué significa FIFA?",
+	answer_a: "Asociación Internacional Federada de Fútbol",
+	answer_b: "Federación Internacional de Fútbol Amateur",
+	answer_c: "Federación Internacional de Fútbol Asociada",
+	answer_d: "Asociación Internacional de Finales de Fútbol",
+	correct_answer: "c",
+}];
+
+const question_elm=document.querySelector(".question");
+const answer_elm= document.getElementsByClassName("answer");
+const button_elm= document.getElementById("btn");
+const frame= document.querySelector(".form-check");
+
+
+let currentQuestion= 0;
+let score=0;
+var  questionChoosen=(Math.floor(Math.random()*(quizData.length-10)));
+// var questionChoosen= (Math.floor(Math.random()*quizData.length));
+
+
+console.log(Math.random()*quizData.length);
+console.log(questionChoosen);
+load_Quiz();
+
+
+
+function load_Quiz(){
+	
+	getDeselected();
+	const questionloaded= quizData[questionChoosen];
+
+	question_elm.innerHTML= questionloaded.question;
+	answer_elm[0].innerHTML=questionloaded.answer_a;
+	answer_elm[1].innerHTML=questionloaded.answer_b;
+	answer_elm[2].innerHTML=questionloaded.answer_c;
+	answer_elm[3].innerHTML=questionloaded.answer_d;
+};	
+
+
+function getSelected(){
+
+	const answer_element= document.querySelectorAll(".answer_input");
+	let answer= undefined;
+	answer_element.forEach((answerEl)=>{
+		console.log(answerEl);
+		if (answerEl.checked){
+			answer=answerEl.id;
+			console.log(answer);
+		}
+	
+	});
+	return answer;
+}
+function getDeselected(){
+	const answer_element= document.querySelectorAll(".answer_input");
+	answer_element.forEach((answerEl)=>{
+		answerEl.checked=false;
+	});
+}
+
+button_elm.addEventListener("click", ()=>{
+
+	const answer=getSelected();
+	if(answer){
+		console.log(quizData[questionChoosen].correct_answer);
+		console.log(answer);
+	 	if(answer===quizData[questionChoosen].correct_answer){
+	 		score++;
+	 	}
+	 	currentQuestion++;
+	 	questionChoosen++;
+	 		
+	 	if (currentQuestion < 10){
+	 		load_Quiz();
+	 	}else{
+	 		frame.innerHTML=` <h3>Tu puntuación es ${score}/10 </h3> 
+	 						<button id="last-btn"> Empezar nuevo Trivial</button>`;
+	 		reloadBtn=document.getElementById("last-btn");
+	 		reloadBtn.addEventListener("click", ()=>{
+	 			location.reload();
+	 			return false;
+	 		});
+	 		// alert("Tu puntuacion es "+score+"/"+quizData.length);
+	 	}
+	}
+
+});
+
+
